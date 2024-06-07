a924585dd963d5cb4438b90365c73802c4969d2d
soham-mehta <83899822+soham-mehta@users.noreply.github.com>
2024-06-07 17:17:23 +0530

    Update top_k_freq_elements.cpp

--- top_k_freq_elements.cpp
+++ top_k_freq_elements.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
 
+        std::map<int, int> numCount;
+         for (int num : nums) {
+            if (numCount.find(num) != numCount.end()) {
+                // If the key exists, increment the value
+                numCount[num]++;
+            } else {
+            numCount[num] = 1;
+            }
+        }
+
+        std::vector<std::pair<int, int>> countVec(numCount.begin(), numCount.end()); 
+
+        std::sort(countVec.begin(), countVec.end(), compareByValue);
+
+        std::vector<int> result;    
+        for (int i = 0; i < k && i < countVec.size(); ++i) {        
+            result.push_back(countVec[i].first);    
+        }
+
+        return result;
+
+        
+        
+    }
+
+    static bool compareByValue(const std::pair<int, int>& a, const std::pair<int, int>& b) {
+    return a.second > b.second;
+}
+};
